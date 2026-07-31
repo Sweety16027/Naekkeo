e603becfd2cf5fb6860201ee8b64ee8ca36f0043
Sweety16027 <sweetyjo.1601@gmail.com>
2026-07-31 16:08:14 +0530

    Create romantic webpage layout in index.html.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function createHeart() {
 
+const heart = document.createElement("div");
+
+heart.className = "heart";
+
+heart.innerHTML = "❤️";
+
+heart.style.left = Math.random() * 100 + "vw";
+
+heart.style.animationDuration = (Math.random() * 3 + 5) + "s";
+
+heart.style.fontSize = (Math.random() * 15 + 18) + "px";
+
+document.body.appendChild(heart);
+
+setTimeout(() => {
+    heart.remove();
+}, 8000);
+
+}
+
+setInterval(createHeart, 300);
+
+// Welcome Message
+
+window.onload = function(){
+
+alert("❤️ Welcome to Naekkeo ❤️");
+
+};
